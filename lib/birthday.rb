193b303410e6c5d0352e95595ce17bd41cf9c008
Samara Grant <samara.amanda01@gmail.com>
2020-07-18 01:31:36 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,6 +9,7 @@ def happy_birthday(birthday_kids)
   if age <= 12
     birthday_kids.each do |kids_name, age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  else 
+    end
+  end
   birthday_kids
 end
